00117c6437da9eb67df46dcd0fb51dda14c17fbb
Samuel Thauvin <samuel;thauvin@findnorder.com>
2023-02-06 00:08:02 +0100

    [FIX] make systeme and item work and add a new type of item

--- module/shayo-foundry.js
+++ module/shayo-foundry.js
@@ -1,6 +1,7 @@
-import ShayoItemSheet from "./sheets/shayo-item";
+import ShayoItemSheet from "./sheets/shayoitemsheet.js";
 
 Hooks.once("init", () => {
+    console.log(`Shayo || init système`);
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("shayo-foundry", ShayoItemSheet, { makeDefault: true });
 })
